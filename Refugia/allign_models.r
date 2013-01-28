51577e4d856f0f24fe36bae4c84f3ea1937ec0d1
danielrosauer <danielrosauer@394b1998-26ba-c760-0df3-ebdbcbab81c8>
2013-01-28 22:58:09 +0000

    allign_models.r ensures that each model has exactly the same spatial extent

--- Refugia/allign_models.r
+++ Refugia/allign_models.r
@@ -4,16 +4,18 @@ library(raster)
 
 #define directories
 input.dir   = 'C:/Users/u3579238/Work/Phylofest/Models/combined/lineage_models/'; setwd(input.dir)
-output.dir  = 'C:/Users/u3579238/Work/Phylofest/Models/combined/lineage_models_ext/'
+output.dir  = 'C:/Users/u3579238/Work/Phylofest/Models/combined/lineage_models_aligned/'
 template_ext ='C:/Users/u3579238/GISData/EnvironmentGrids/AusGDMGrids/ForMaxent/bio1.asc'
 
 input_files= list.files(path = input.dir, full.names = FALSE, recursive = FALSE)
-output_files= list.files(path = output.dir, pattern = "*.asc.gz", recursive = FALSE,ignore.case = TRUE, include.dirs = FALSE)
+output_files= list.files(path = output.dir, pattern = "*.asc", recursive = FALSE,ignore.case = TRUE, include.dirs = FALSE)
 
 template.asc = read.asc(template_ext)
 template.ras = raster.from.asc(template.asc)
 new_extent =      extent(template.ras)
 new_extent@xmin = max(new_extent@xmin, 140.01)
+
+new_only <- TRUE  # if true, skip grids which are already in the output directory
 
 for (tfile in input_files) {
   checkname=unlist(strsplit(tfile,".",fixed=T))
@@ -22,8 +24,8 @@ for (tfile in input_files) {
     pathname = tfile
     dataname = unlist(strsplit(pathname,".asc")[1])[1]  #define the column name
     outname = paste(output.dir,dataname,sep="")
-    
-    if (! paste(dataname,".asc",sep='') %in% output_files) {
+
+    if ((!paste(dataname,".asc",sep='') %in% output_files) | (! new_only)) {
       grid.asc = read.asc(filepath) #read in the data
       grid.ras = raster.from.asc(grid.asc)
       grid_ext.ras = extend(grid.ras,new_extent,value=0) # extend to the union of current grid and new extent
