6599e7c911bb234145786a2ef8064e3bd25230ff
danielrosauer <danielrosauer@394b1998-26ba-c760-0df3-ebdbcbab81c8>
2013-02-10 13:10:26 +0000

    Veg grids now have 1 for rainforest, 0 for other land (current) and NA for non-land

--- stability_scripts/Oz/subset_veg.r
+++ stability_scripts/Oz/subset_veg.r
@@ -1,5 +1,4 @@
-#drafted by Jeremy VanDerWal ( jjvanderwal@gmail.com ... www.jjvanderwal.com )
-#GNU General Public License .. feel free to use / distribute ... no warranties
+#drafted by Dan Rosauer, using elements from a script by Jeremy VanDerWal ( jjvanderwal@gmail.com ... www.jjvanderwal.com )
 
 ################################################################################
 library(SDMTools, raster)
@@ -8,39 +7,44 @@ library(SDMTools, raster)
 ################################################################################
 
 #define directories
-template = 'C:/Users/u3579238/Work/Refugia/Stability/NVIS/meq_rainforest2.asc'
+template = 'C:/Users/u3579238/Work/Refugia/Stability/OZ.climates/bioclim/000/landmask.asc'
 #veg_grid = 'C:/Users/u3579238/Work/Refugia/Stability/Oz/pre1750_mvg.asc.gz'
-veg_grid = 'C:/Users/u3579238/Work/Refugia/Stability/NVIS/nvis4_1_mvs_pre_geo_4km_trop_rf_only.asc'
-output_grid = 'C:/Users/u3579238/Work/Refugia/Stability/NVIS/cec_rainforest.asc'
+veg_grid = 'C:/Users/u3579238/Work/Refugia/Stability/NVIS/nvis4_1_mvs_pre_geo_4km_rf_only.asc'
+output_grid = 'C:/Users/u3579238/Work/Refugia/Stability/NVIS/CEC_all_rainforest.asc'
 
 #define some basic data
-mvg.asc = read.asc(veg_grid)                 # read in the vegetation grid
-rf.asc = mvg.asc; rf.asc[which(is.finite(rf.asc) & rf.asc!=1)] = 0  #set all veg != 1 (rainforests) to 0
+mvs.asc = read.asc(veg_grid)                 # read in the vegetation grid
+template.asc=read.asc(template)
+rf.asc = mvs.asc 
+#rf.asc[which(is.finite(rf.asc) & !(rf.asc==1 | rf.asc==2 | rf.asc==6))] <- 0  #set all veg != 1 (rainforests) to 0
+rf.asc[which(is.finite(rf.asc))] <- 1  #set all rainforest to 1
 
 ################################################################################
 #get a subset of the data for occur & background
-pos = as.data.frame(which(is.finite(mvg.asc),arr.ind=TRUE)) #get all points that have data
-pos$mvg = mvg.asc[cbind(pos$row,pos$col)] #append the vegetation data
+pos = as.data.frame(which(is.finite(rf.asc),arr.ind=TRUE)) #get all points that have data
+pos$rf = rf.asc[cbind(pos$row,pos$col)] #append the vegetation data
 
-mvg.ras = raster.from.asc(mvg.asc)
+rf.ras = raster.from.asc(rf.asc)
 
-old_extent = extent(mvg.ras)
+old_extent = extent(rf.ras)
 
 ymax=-23.8
-ymin=-33.0
+ymin=-32.5
 
 smaller_extent = old_extent
 smaller_extent@ymax = ymax
 smaller_extent@ymin = ymin
 
-template.asc=read.asc(template)
 template.ras=raster.from.asc(template.asc)
 template.extent=extent(template.ras)
 
-mvg_new.ras = crop(mvg.ras,smaller_extent)
-mvg_new.ras = extend(mvg_new.ras,template.extent,0)
-mvg_new.ras = crop(mvg_new.ras,template.extent)
-mvg_new.asc = asc.from.raster(mvg_new.ras)
-mvg_new.asc[which(is.na(template.asc))] = NA
+rf_new.ras = crop(rf.ras,smaller_extent)
+rf_new.ras = extend(rf_new.ras,template.extent,0)
+rf_new.ras = crop(rf_new.ras,template.extent)
+rf_new.asc = asc.from.raster(rf_new.ras)
+rf_new.asc[which(is.na(template.asc))] = NA
 
-write.asc(mvg_new.asc,output_grid)
+rf_new.asc[is.na(rf_new.asc) & !is.na(template.asc)] <- 0
+
+write.asc(rf_new.asc,output_grid)
+cat("\nWritten",output_grid,"\n")
