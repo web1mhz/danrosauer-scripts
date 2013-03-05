8e4d997fd317cf34d1cd329d73fa682a0a5ff44f
danielrosauer <danielrosauer@394b1998-26ba-c760-0df3-ebdbcbab81c8>
2013-03-05 01:37:23 +0000

    Now uses either all grids in the folder, or a specified list

--- GDM/GDM_table_prepare.r
+++ GDM/GDM_table_prepare.r
@@ -10,25 +10,38 @@ geogdist = function(x1,y1,x2,y2) {
 }
 
 ########## Parameters ##########
-#inputs
-work.dir          <- 'C:/Users/Dan/GDM_R/'
-env.dir           <- 'C:/Users/Dan/Work/GISData/AsciiGrids/test_set'
-site_pair_file    <- 'C:/Users/Dan/Study_Taxa/Herps/Myobatrachidae/phylo_dist_Myob_chrono_0.4_test.csv'
-output_file       <- 'sitepairs_env.csv'
+software          <- 'C:/Users/u3579238/Work/GDM/software/GDM4Tables_1_0/GDM_Table_Funcs64_1_0.R'
+work.dir          <- 'C:/Users/u3579238/Work/GDM/GDM_table_test/'
+env.dir           <- 'C:/Users/u3579238/GISData/EnvironmentGrids/AusGDMGrids/AsciiGrids/'
+site_pair_file    <- 'C:/Users/u3579238/Work/Study_Taxa/Herps/Myobatrachidae/species&phylo_Myob_chrono_0.4.csv'
+output_file       <- 'sitepairs_env1.csv'
+dist.fieldname    <- 'phylo_sorenson'
+
+grids_to_use      <- c("microgi.asc","mesogi.asc","C4gi.asc","rhu215_x.asc","arid_max.asc","rprecmax.asc","slrain1.asc","slrain2.asc","radni.asc","radnx.asc","mintx.asc","rtxmin.asc","distpermwat_limit1.asc","gravity.asc","geollrngeage.asc","nmnln0.asc","ridgetopflat.asc","slope.asc","mvs31_ccrf.asc")
+# comment the precding line and get all grids in the directory if grids_to_use doesn't exist
 ################################
 
 setwd(work.dir)
+source(software)
 
 input.sitepairs   <- read.csv(site_pair_file)
-new.sitepairs     <- input.sitepairs[c("x0","y0","x1","y1","dist")]
+names(input.sitepairs)[which(names(input.sitepairs)==dist.fieldname)] <- "observed"
+new.sitepairs     <- input.sitepairs[c("observed","x0","y0","x1","y1")]
+new.sitepairs["weight"] <- rep.int(1,nrow(new.sitepairs))
 
 site0_xy <- new.sitepairs[c("x0","y0")]
 site1_xy <- new.sitepairs[c("x1","y1")]
 
+# this gets all grids in the floder, if none specified
+if (!exists("grids_to_use")) {
+  grids_to_use <- list.files(env.dir,pattern='*.asc',full.name=TRUE)
+}
+
 #extract values from each environmental layer in the folder
-for (tfile in list.files(env.dir,pattern='*.asc',full.name=TRUE)) {
+for (tfile in grids_to_use) {
   cat("\nabout to do", tfile)
-  tasc = read.asc(tfile) #read in the data
+  file_path = paste(env.dir,tfile,sep="")
+  tasc = read.asc(file_path) #read in the data
   dataname = gsub(env.dir,'',tfile);  
   dataname = gsub('\\_msk.asc','',dataname)
   dataname = gsub('/','',dataname) #define the column name
@@ -36,16 +49,23 @@ for (tfile in list.files(env.dir,pattern='*.asc',full.name=TRUE)) {
   colname0  = paste("site0.",dataname,sep="")
   colname1  = paste("site1.",dataname,sep="")
   new.sitepairs[colname0] = extract.data(site0_xy,tasc) #append the data
-  new.sitepairs[colname1] = extract.data(site1_xy,tasc) #append the data  
+  new.sitepairs[colname1] = extract.data(site1_xy,tasc) #append the data
 }
 
 # reorder columns
-columns <- c("dist","x0","y0","x1","y1",sort(names(new.sitepairs)[6:ncol(new.sitepairs)]))
+columns <- c("observed","weight","x0","y0","x1","y1",sort(names(new.sitepairs)[7:ncol(new.sitepairs)]))
 new.sitepairs <- new.sitepairs[columns]
 
 new.sitepairs <- na.omit(new.sitepairs)
 
+below.zero    <- which(new.sitepairs$observed<0)
+cat("\n",length(below.zero),"records removed due to a dissimilarity < 0\n")
+new.sitepairs <- new.sitepairs[which(new.sitepairs$observed>=0),]
+
+
 cat("\nAbout to write table to file\n")
 write.csv(new.sitepairs,output_file)
 
-cat("\nFinished\n")
+rm(input.sitepairs, site0_xy, site1_xy)
+
+cat("\nFinished at",date(),"\n")
