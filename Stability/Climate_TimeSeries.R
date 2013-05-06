51248fb523183aaa30258abc9cf25839229bad4e
danielrosauer <danielrosauer@394b1998-26ba-c760-0df3-ebdbcbab81c8>
2013-05-06 06:29:17 +0000

    Updates to stability scripts

---
+++ Stability/Climate_TimeSeries.R
@@ -0,0 +1,51 @@
+
+library(SDMTools)
+source("ClimateByTime.r")
+
+# set the parameters
+env.dir         <- "C:/Users/u3579238/Work/Refugia/Stability/OZ.climates/bioclim"
+model.dir       <- "C:/Users/u3579238/Work/Refugia/Stability/AWT_RF/maxent.output8"
+stability.dir   <- "C:/Users/u3579238/Work/Refugia/Stability/AWT_RF/stability.8"
+stability.file  <- "static.sum.cost.asc"
+
+x <- 145.8
+y <- -17.3
+layer <- "bioclim_12.asc.gz"
+env_value_name <- "annual precipitation (mm)"
+
+# get the climate time series
+times <- point.time.series(x,y,layer,env.dir)
+times$yearnum <- as.numeric(as.character(times$year))
+
+#get the model time series
+models <- model.time.series(x,y,layer,model.dir)
+models$yearnum <- as.numeric(as.character(models$year))
+
+#get the stability value
+grid_path     <- paste(stability.dir,stability.file,sep="/")
+stability.asc <- read.asc(grid_path)
+point <- data.frame(x,y)
+stability     <- extract.data(point,stability.asc)
+
+#draw the time series graph
+windows(10,7)
+par(mar=c(5,4,4,5)+.1)
+
+header <- paste("Time series of",env_value_name,"since 120kya\nLatitude:",y,"Longitude:",x)
+yrange <- max(times$value) - min(times$value)
+ymin <- min(times$value) - yrange/6
+ymax <- max(times$value) + yrange/6
+
+plot(times$yearnum,times$value,xlim=c(120,0),ylim=c(ymin,ymax),type="l",col="red", lwd=2,main=header,ylab=env_value_name,xlab="thousands of years before present", cex.lab=1.3, cex.axis=1.3)
+abline(h=times$value[1],lty=2,col="red",lwd=2)
+
+model_value_name <- "habitat suitability"
+par(new=TRUE)
+plot(models$yearnum, models$value,,type="l",col="black",xaxt="n",yaxt="n",xlab="",ylab="",lty=2,lwd=2,xlim=c(120,0),ylim=c(0,1.07))
+axis(4,cex.axis=1.3)
+mtext(model_value_name,side=4,line=3, cex=1.5)
+
+abline(h=stability,lwd=2,col="blue")
+
+legend("topleft",col=c("red","red","black","blue"),lty=c(1,2,2,1),lwd=2,cex=1.2,legend=c(env_value_name,"current environment",model_value_name,"stability - static"),bg="white")
+
