250ed4b6151e86c974e214dc7e446b3e5238eb71
danielrosauer <danielrosauer@394b1998-26ba-c760-0df3-ebdbcbab81c8>
2013-09-17 00:25:44 +0000

    align_models.r simplified to use just the raster library - quicker too

--- Refugia/align_models.r
+++ Refugia/align_models.r
@@ -1,5 +1,4 @@
-
-library(SDMTools)
+rm(list=ls())
 library(raster)
 
 #define directories
@@ -12,28 +11,24 @@ input_files= list.files(path = input.dir, pattern = file.pattern, full.names = F
                         ignore.case = TRUE, include.dirs = FALSE)
 output_files= list.files(path = output.dir, pattern = file.pattern, recursive = FALSE,ignore.case = TRUE, include.dirs = FALSE)
 
-template.asc = read.asc(template_ext)
-template.ras = raster.from.asc(template.asc)
+template.ras = raster(template_ext)
 new_extent =      extent(template.ras)
 new_extent@xmin = max(new_extent@xmin, 140.01)
 
-new_only <- TRUE  # if true, skip grids which are already in the output directory
+new_only <- FALSE  # if true, skip grids which are already in the output directory
 
 for (tfile in input_files) {
+#for (tfile in input_files[85:length(input_files)]) {
   filepath=paste(input.dir,tfile,sep='')
-  pathname = tfile
-  dataname = unlist(strsplit(pathname,".asc")[1])[1]  #define the column name
-  outname = paste(output.dir,dataname,sep="")
+  outname = paste(output.dir,tfile,sep="")
 
-  if ((!paste(dataname,".asc",sep='') %in% output_files) | (! new_only)) {
-    grid.asc = read.asc(filepath) #read in the data
-    grid.ras = raster.from.asc(grid.asc)
+  if ((!tfile %in% output_files) | (! new_only)) {
+    grid.ras = raster(tfile)
     grid_ext.ras = extend(grid.ras,new_extent,value=0) # extend to the union of current grid and new extent
     grid_ext.ras = crop(grid_ext.ras,new_extent) # crop back to new extent
-    grid_ext.asc= asc.from.raster(grid_ext.ras)
-    write.asc(grid_ext.asc,outname)
-    cat("\nExtended asc written for",dataname)
+    writeRaster(grid_ext.ras,outname,overwrite=TRUE, NAflag=-9999)
+    cat("\nExtended asc written for",tfile)
   } else {
-    cat("\nSkipped",dataname)
+    cat("\nSkipped",tfile)
   }
 }
