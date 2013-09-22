5c9061da8eeaa687eb04cd8334091b3de5259e27
danielrosauer <danielrosauer@394b1998-26ba-c760-0df3-ebdbcbab81c8>
2013-09-22 03:13:20 +0000

    subset_veg.r splits a veg grid by specific classes and latitudinal bands.  It now also generates mask grids which buffer each veg class by a specified distance (to define the model background)

--- trunk/Stability/Oz/subset_veg.r
+++ trunk/Stability/Oz/subset_veg.r
@@ -1,50 +1,71 @@
 #drafted by Dan Rosauer, using elements from a script by Jeremy VanDerWal ( jjvanderwal@gmail.com ... www.jjvanderwal.com )
 
 ################################################################################
+rm(list=ls())
 library(SDMTools, raster)
-
-################################################################################
-################################################################################
 
 #define directories
 template = 'C:/Users/u3579238/Work/Refugia/Stability/OZ.climates/bioclim/000/landmask.asc'
 #veg_grid = 'C:/Users/u3579238/Work/Refugia/Stability/Oz/pre1750_mvg.asc.gz'
 veg_grid = 'C:/Users/u3579238/Work/Refugia/Stability/NVIS/nvis4_1_mvs_pre_geo_4km_rf_only.asc'
-output_grid = 'C:/Users/u3579238/Work/Refugia/Stability/NVIS/CEC_all_rainforest.asc'
+output_suffix = '_all_rainforest'
+output_path = 'C:/Users/u3579238/Work/Refugia/Stability/NVIS/'
+buffer_width = 200000 # background region around RF pixels, in metres
 
-#define some basic data
-mvs.asc = read.asc(veg_grid)                 # read in the vegetation grid
-template.asc=read.asc(template)
-rf.asc = mvs.asc 
-#rf.asc[which(is.finite(rf.asc) & !(rf.asc==1 | rf.asc==2 | rf.asc==6))] <- 0  #set all veg != 1 (rainforests) to 0
-rf.asc[which(is.finite(rf.asc))] <- 1  #set all rainforest to 1
+region_lat_boundaries <- c(-9.75,-14.85,-19.6,-23.4,-32.44, -44.3)
+region_names          <- c("CYP","AWT","MEQ","CEC","SEA")
 
-################################################################################
-#get a subset of the data for occur & background
-pos = as.data.frame(which(is.finite(rf.asc),arr.ind=TRUE)) #get all points that have data
-pos$rf = rf.asc[cbind(pos$row,pos$col)] #append the vegetation data
-
-rf.ras = raster.from.asc(rf.asc)
-
-old_extent = extent(rf.ras)
-
-ymax=-23.8
-ymin=-32.5
-
-smaller_extent = old_extent
-smaller_extent@ymax = ymax
-smaller_extent@ymin = ymin
-
-template.ras=raster.from.asc(template.asc)
+#################################################################
+template.ras=raster(template)
 template.extent=extent(template.ras)
 
-rf_new.ras = crop(rf.ras,smaller_extent)
-rf_new.ras = extend(rf_new.ras,template.extent,0)
-rf_new.ras = crop(rf_new.ras,template.extent)
-rf_new.asc = asc.from.raster(rf_new.ras)
-rf_new.asc[which(is.na(template.asc))] = NA
+for (region in 1:length(region_names)) {
+  ymax = region_lat_boundaries[region]
+  ymin = region_lat_boundaries[region+1]
+  region_name = region_names[region]
 
-rf_new.asc[is.na(rf_new.asc) & !is.na(template.asc)] <- 0
+  output_veg  <- paste(output_path,region_name,output_suffix,'.asc',sep='')
+  output_mask <- paste(output_path,region_name,output_suffix,'_mask.asc',sep='')
 
-write.asc(rf_new.asc,output_grid)
-cat("\nWritten",output_grid,"\n")
+  
+  mvs.asc = read.asc(veg_grid)                 # read in the vegetation grid
+  rf.asc = mvs.asc 
+  #rf.asc[which(is.finite(rf.asc) & !(rf.asc==1 | rf.asc==2 | rf.asc==6))] <- 0  #set all veg != 1 (rainforests) to 0
+  rf.asc[which(is.finite(rf.asc))] <- 1  #set all rainforest to 1
+
+  #get a subset of the data for occur & background
+  pos = as.data.frame(which(is.finite(rf.asc),arr.ind=TRUE)) #get all points that have data
+  pos$rf = rf.asc[cbind(pos$row,pos$col)] #append the vegetation data
+  rf.ras = raster.from.asc(rf.asc)
+  
+  old_extent = extent(rf.ras)
+  
+  smaller_extent = old_extent
+  smaller_extent@ymax = ymax
+  smaller_extent@ymin = ymin
+  
+  rf_new.ras = crop(rf.ras,smaller_extent)
+  rf_new.ras = extend(rf_new.ras,template.extent,0)
+  rf_new.ras = crop(rf_new.ras,template.extent)
+  rf_new.ras[is.na(template.ras)] = NA
+  rf_new.ras[is.na(rf_new.ras) & !is.na(template.ras)] <- 0
+  
+  writeRaster(rf_new.ras,output_veg,overwrite=TRUE)
+  cat("\nWritten",output_veg,"\n")
+  
+  rm(rf.asc, rf.ras)
+  
+  # now create a buffered mask for this rf region
+  region.ras <- raster(output_veg)
+  region.ras[region.ras==0] <- NA
+  region_buf.ras <- buffer(region.ras,width=buffer_width, doEdge=TRUE)
+  region_buf.ras <- mask(region_buf.ras,template.ras)
+  writeRaster(region_buf.ras,output_mask,overwrite=TRUE)
+  cat("Written",output_mask,"\n")
+  
+  windows(8,8)
+  plot(region_buf.ras,xlim=c(140,160))
+  
+  Sys.sleep(1)
+  
+}
