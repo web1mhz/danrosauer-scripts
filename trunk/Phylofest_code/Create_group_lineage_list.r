54f3fd0e9df06fa9b88348657a7eb669cafae5c8
danielrosauer <danielrosauer@394b1998-26ba-c760-0df3-ebdbcbab81c8>
2014-04-09 01:24:11 +0000

    Edits to the scripts to get PE, PD etc from models.  Note, phylogenetic diversity from models.r is not working, but committing to support later rollback if needed.  The problem is with differences in the SDMTools:asc and raster:raster formats

--- trunk/Phylofest_code/Create_group_lineage_list.r
+++ trunk/Phylofest_code/Create_group_lineage_list.r
@@ -1,16 +1,29 @@
 # create group_species_lineage_.csv
 
-base.dir  <-  'C:/Users/Dan/Work/AMT/Models/'
+rm(list=ls())
+
+base.dir  <-  'C:/Users/u3579238/Work/AMT/Models/'
 work.dir  <-  paste(base.dir,"lineage_sites",sep="")
-genus     <-  'Gehyra'
-in.file.name    <-  paste(genus,"_lin_loc.csv",sep="")
-out.file.name   <-  paste(base.dir,"group_lineage_list.csv",sep="")
+genus     <-  'Heteronotia'
+#in.file.name    <-  paste(genus,"_lin_loc.csv",sep="")
+in.file.name <- "Heteronotia_lin_loc_from_db_18mar14_nth_of_22S.csv"
+out.file.name   <-  paste(base.dir,"Heteronotia_binoei_lineage_list.csv",sep="")
 
 setwd(work.dir)
 
 tab <- read.csv(in.file.name)
-tab <- tab[,c("Genus","Species","ModelGroup","lineage")]
+tab <- tab[tab$use==1,c("Genus","Species","ModelGroup","lineage")]
 tab <- unique(tab)
+lin_num <- seq(from=1,to=nrow(tab),1)
+tab <- cbind(tab,lin_num)
 
 write.csv(tab,out.file.name,row.names=F)
 
+# now number lineages in lin_loc
+lin_loc <- read.csv(in.file.name)
+lin_loc <- lin_loc[lin_loc$use == 1,]
+lin_loc_merged <- merge(lin_loc,tab[,c("lineage","lin_num")],by='lineage')
+#lin_loc_merged <- lin_loc_merged[,c(2:5,1,6:11,14)]
+#lin_loc_merged <- lin_loc_merged[,c(2:5,1,6:17)]
+#write.csv(lin_loc_merged,paste(genus,"_lin_loc2.csv",sep=""),row.names=FALSE)
+write.csv(lin_loc_merged,"Heteronotia_lin_loc_from_db_18mar14_nth_of_22S_numbered.csv",row.names=FALSE)
--- trunk/Phylofest_code/phylogenetic diversity from models.r
+++ trunk/Phylofest_code/phylogenetic diversity from models.r
@@ -4,8 +4,9 @@ rm(list=ls())
 
 library(SDMTools)
 library(raster)
+library(ape)
 library(phylobase)
-source("C:/Users/Dan/Work/Software/Phylofest_code/phylogenetic endemism.r")
+source("C:/Users/u3579238/Work/Software/danrosauer-scripts/Phylofest_code/phylogenetic endemism.r")
 
 ################################################################################
 #first define some functions
@@ -27,39 +28,45 @@ image.grid = function(tasc,tfile,zlim=NULL,cols=NULL) {
 ################################################################################
 ################################################################################
 
-#TEMP
-#row.limit=20000
-#TEMP
+max.rows=10000000
 
 #define directories
-base.dir   =    'C:/Users/Dan/Work/AMT/Models/'
-input.dir       <- 'lineage_models/asc_clipped'
+base.dir   =    'C:/Users/u3579238/Work/AMT/Models/'
+#input.dir       <- 'lineage_models/asc_clipped'
+input.dir       <- 'lineage_models/asc_clipped_cube_method/'
 output.dir      <- base.dir
-file_suffix     <- '.asc'
-template_grid   <- 'C:/Users/Dan/Work/AMT/Models/AMT_template.asc'
-group_lin_list  <- 'group_lineage_list.csv'
+file_pattern    <- 'lin_model_het'
+#file_pattern    <- 'gehyra_'
+template_grid   <- 'C:/Users/u3579238/Work/AMT/Models/species_models/maxent/Heteronotia/heteronotia_binoei_17mar14_mean.asc'
+#group_lin_file  <- 'group_lineage_list.csv'
+group_lin_file  <- 'Heteronotia_binoei_lineage_list.csv'
 
 #tree details  - this works for one genus at a time
-tree.file     = 'trees/Gehyra uniq lineages 141113 final.tre'
-outgroup      = 'Heteronotia'
+#tree.file     = 'trees/TreeLineagesBinoei181113.nex'
+tree.file     = 'trees/TopEnd_8Jan14_strictclock_relabel.nex'
+outgroup      = 'planiceps'
+#outgroup      = 'heteronotia'
 preface       = 'lin_model_'
 
-output_prefix <- "gehyra_"
+#output_prefix <- "het_"
+output_prefix <- "Hb_"
 threshold = 0.01  # this is not a species level threshold, but one used for each lineage model
 
 ####  end of parameters
 
 setwd(base.dir)
-files = list.files(path = input.dir, pattern = file_suffix, recursive = FALSE,ignore.case = TRUE, include.dirs = FALSE)
+files <- list.files(path = input.dir, pattern = file_pattern, recursive = FALSE,ignore.case = TRUE, include.dirs = FALSE)
 
 setwd(input.dir)
 #template.asc = read.asc.gz(files[1])
-template.asc = read.asc(template_grid)
-model_rows=nrow(template.asc)
-model_cols=ncol(template.asc)
+template.ras = raster(template_grid)
+model_rows=nrow(template.ras)
+model_cols=ncol(template.ras)
 
 # the original version, excluding NA cells
-pos <- as.data.frame(which(is.finite(template.asc),arr.ind=TRUE)) #get all points that have data
+finite_cells <- which(is.finite(template.ras[]))
+pos <- as.data.frame(rowColFromCell(template.ras,finite_cells)) #get all points that have data
+rm(finite_cells)
 
 i <- 0
 
@@ -67,16 +74,14 @@ for (tfile in files) {
   checkname=unlist(strsplit(tfile,".",fixed=T))
   if (checkname[length(checkname)]=="asc") {   # only accept filenames ending in .asc
     #tasc = read.asc.gz(tfile)                            #read in the data
-    tasc = read.asc(tfile)                                #read in the data    
-    dataname=gsub(file_suffix,'',tfile)
-    if (dataname != "Cophixalus_peninsularis") {              #skipping a dodgy model
-      newname <- tolower(gsub(preface,"",dataname))
-      pos[newname] <- tasc[cbind(pos$row,pos$col)]           #append the data
-      pos[(which(pos[newname]< threshold)),newname] <- 0    # set values below the threshold to 0
-      pos[(which(is.na(pos[newname]))),newname]     <- 0    # set the nulls to 0    
-      i <- i+1
-      cat("\n",i,newname,"loaded")
-    }   
+    tras = raster(tfile)                                #read in the data    
+    newname <- gsub(".asc",'',tfile)
+    newname <- tolower(gsub(preface,"",newname))
+    pos[newname] <- tras[cbind(pos$row,pos$col)]           #append the data
+    pos[(which(pos[newname]< threshold)),newname] <- 0    # set values below the threshold to 0
+    pos[(which(is.na(pos[newname]))),newname]     <- 0    # set the nulls to 0    
+    i <- i+1
+    cat("\n",i,newname,"loaded")
   }
 }
 
@@ -86,97 +91,81 @@ rowsums <- apply(pos[,3:ncol(pos)],1,sum,na.rm=T)
 pos <- pos[which(rowsums>0),]
 rm(rowsums)
 cat("After:",nrow(pos),"\n")
+max.rows <- min(max.rows,nrow(pos))
 gc()
 
 setwd(base.dir)
-group_lin_list <-read.csv(group_lin_list)
+group_lin_list <-read.csv(group_lin_file)
+group_lin_list$lineage <- tolower(group_lin_list$lineage)
 
 # read in the tree
-tree <- read.tree(tree.file)
-tree <- drop.tip(tree,tip=which(tree$tip.label==outgroup))
+tree_suffix <- unlist(strsplit(tree.file,"[.]"))[2]
+if (tree_suffix == "nex") {
+  tree <- read.nexus(tree.file)
+} else {
+  tree <- read.tree(tree.file)
+}
+if (outgroup != '') {
+  tree <- drop.tip(tree,tip=which(tree$tip.label==outgroup))
+}
 tree <- phylo4(tree)
+labels(tree) <- tolower(labels(tree))
 
 # ensure that the tree tips match the model names
-model.names <- names(pos[3:(i+2)])  # names of all columns except the 1st two which are row, col
+model.names <- names(pos)
+model.names <- model.names[-(1:2)] # names of all columns except the 1st two which are row, col
 #model.names <- tolower(gsub(preface,"",model.names))
-model.groups <- vector("character",1)
+model.groups <- data.frame(model.groups=vector("character",nTips(tree)),stringsAsFactors=F)
 
 for (i in 1:nTips(tree)) {
+  cat(labels(tree)[i],"\n")
   row <- group_lin_list[group_lin_list$lineage==labels(tree)[i],]
-  labels(tree)[i] <- tolower(paste(row$ModelGroup,row$lineage,sep="_"))
-  model.groups[i] <- as.character(row$ModelGroup)
+  if (nrow(row) > 0) {
+    labels(tree)[i] <- tolower(paste(row$ModelGroup,row$lineage,sep="_"))
+    model.groups[i,1] <- as.character(row$ModelGroup)
+  }
 }
+
+tree <- phylo4d(tree,tip.data=model.groups)
 
 tree.names  <- as.character(labels(tree)[1:nTips(tree)])
 matched.names <- intersect(model.names,tree.names)
-cat("Not in tree names:",setdiff(model.names,tree.names))
-cat("Not in model names:",setdiff(tree.names,model.names))
+matched.tips  <- which(labels(tree,"tip") %in% matched.names)
+cat("\nNot in tree names:",setdiff(model.names,tree.names),"\n")
+cat("\nNot in model names:",setdiff(tree.names,model.names),"\n")
+
+# a subtree containing only the tips for which there is a corresponding model
+subtree <- subset(tree,tips.include=matched.tips)
+
+write.csv(pos,paste(output_prefix,"sites_x_lineage.csv",sep=''))
 
 gc()
-#result <- calc_PE(tree,pos[1:row.limit,which(names(pos) %in% tree.names)],"probability")
-#result <- calc_PE{(tree,pos[,which(names(pos) %in% tree.names)],"probability")
-#result <- calc_PE_mymodels(tree,pos[1:row.limit,which(names(pos) %in% tree.names)],model.groups)
-result <- calc_PE_mymodels(tree,pos[,which(names(pos) %in% tree.names)],model.groups)
+result <- calc_PE_mymodels(subtree,pos[1:max.rows,which(names(pos) %in% matched.names)])
 gc()
 
-# create a data frame of branches with
-  # a) branch name - equalling column name in pos for the tips
-  # b) branch length
-  # c) model group for tip
-        # and a list of model groups for internal branches?
-
-# loop through the non-tip branches, and for each
-  # add a column to pos
-  # identify all the descendent tips
-  # sum the values within each tip (for each row = grid cell) 
-  # combine the values between model groups using:
-        # 1 - sum(1 - model value)
-  # this is the branch model value
-
-# scale the value of each column to sum to its branch length (instead of to 1 for species model)
-
-# row sums are analogous to PD
-# PE is the sum across the row of cell/column total
-
-# cat("\nData loaded, now performing calculations")
-# 
-# # add the suitability values across all models
-# cols = ncol(pos)
-# pos$total = apply(pos[3:cols],1,'sum')      # analogous to richness
-# 
-# # the endemism calculation
-# species_sums = apply(pos[3:cols],2,'sum')
-# pos_end = pos[1:cols]
-# for (species in names(pos[3:cols])) {
-#   if (species_sums[species] > 0) {    # avoid division by zero if a species column sums to 0
-#     pos_end[species] <- (pos_end[species] / species_sums[species])
-#   }
-# }
-# 
-# pos$we = apply(pos_end[3:cols],1,'sum')
-# rm(pos_end)
-
-pos_output <- cbind(pos[,2:1],result)
+pos_output <- cbind(pos[1:max.rows,1:2],result)
 pos_output <- pos_output[,-3] # omit the site column
+
+# flip the y values
+#pos_output$row <- max(pos_output$row) + 1 - pos_output$row
 
 # add residual columns
 PE_WE_mod <- lm(pos_output$PE~pos_output$WE)
 pos_output$PE_WE_resid <- PE_WE_mod$residuals
 PE_WE_loglog_mod <- lm(log(pos_output$PE)~log(pos_output$WE),subset=which(!is.infinite(log(pos_output$WE))))
 pos_output_log <- cbind(pos_output[which(!is.infinite(log(pos_output$WE))),],PE_WE_loglog_mod$residuals)
+
 dataframe2asc(pos_output_log[,c(1,2,8)],paste(output_prefix,"PE_WE_loglog_resid.asc",sep=""),output.dir)
 
+filenames <- c(paste(output_prefix,"PE.asc",sep=""),paste(output_prefix,"PD.asc",sep=""),paste(output_prefix,"WE.asc",sep=""),paste(output_prefix,"SR.asc",sep=""),paste(output_prefix,"PE_WE_resid.asc",sep=""))
+dataframe2asc(pos_output[,1:7],filenames,output.dir)
 
-
-filenames <- c(paste(output_prefix,"PE.asc",sep=""),paste(output_prefix,"PD.asc",sep=""),paste(output_prefix,"WE.asc",sep=""),paste(output_prefix,"SR.asc",sep=""),paste(output_prefix,"PE_WE_resid.asc",sep=""),paste(output_prefix,"PE_WE_loglog_resid.asc",sep=""))
-dataframe2asc(pos_output,filenames,output.dir)
-
-write.csv(pos_output,"gehyra_scores.csv")
+write.csv(pos_output,paste(output_prefix,"scores.csv",sep=""),row.names=FALSE)
 
 # make some output images
 PE.ras <- raster(filenames[1])
 windows(9,9)
-plot(PE.ras,main="PE",col=rainbow(25,start=0.1,end=1),ylim=c(180,1400),xlim=c(700,2800))
+plot(PE.ras,main="PE",col=rainbow(25,start=0.1,end=1))
 
 PD.ras <- raster(filenames[2])
 windows(9,9)
@@ -216,6 +205,5 @@ pos_output$PE_WE_resid <- PE_WE_mod$residuals
 windows(10,10)
 plot(log(pos_output$WE),log(pos_output$PE),cex=0.7,xlab="WE",ylab="PE")
 
-
-PE.tasc <- asc.from.raster(PE.ras)
-image.grid(PE.tasc,filenames[1],zlim=c(0,1),cols=rainbow(10)) #plot the image after logging the actual data
+# PE.tasc <- asc.from.raster(PE.ras)
+# image.grid(PE.tasc,filenames[1],zlim=c(0,1),cols=rainbow(10)) #plot the image after logging the actual data
