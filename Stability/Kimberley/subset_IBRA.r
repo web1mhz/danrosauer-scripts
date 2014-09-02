4f96026f4c07649c583057b8e65b87cc9c05f1c2
danielrosauer <danielrosauer@394b1998-26ba-c760-0df3-ebdbcbab81c8>
2014-09-02 02:40:23 +0000

    IBRA region models for present day now running error free

--- Stability/Kimberley/subset_IBRA.r
+++ Stability/Kimberley/subset_IBRA.r
@@ -7,9 +7,9 @@ library(raster)
 shp2raster <- function(shp, mask.raster, label="", value, background_value = 0,transform = FALSE, proj.from = NA,
                        proj.to = NA, map = TRUE, write_to_file = FALSE) {
 # This shp2raster function adapted from: http://amywhiteheadresearch.wordpress.com/2014/05/01/shp2raster/
-  
+
   require(raster, rgdal)
-  
+
   # use transform==TRUE if the polygon is not in the same coordinate system as
   # the output raster, setting proj.from & proj.to to the appropriate
   # projections
@@ -17,7 +17,7 @@ shp2raster <- function(shp, mask.raster, label="", value, background_value = 0,t
     proj4string(shp) <- proj.from
     shp <- spTransform(shp, proj.to)
   }
-  
+
   # convert the shapefile to a raster based on a standardised background
   # raster
   r <- rasterize(shp, mask.raster)
@@ -32,12 +32,12 @@ shp2raster <- function(shp, mask.raster, label="", value, background_value = 0,t
   } else {
     r <- mask(merge(r, mask.raster), mask.raster)
   }
-  
+
   # plot map of new raster
   if (map == TRUE) {
     plot(r, main = label, axes = F, box = F)
   }
-  
+
   names(r) <- label
   return(r)
 }
@@ -57,15 +57,12 @@ IBRA_Kimb.shp  <- IBRA.shp[IBRA.shp$REG_NAME_7 %in% region_list,]
 IBRA_Kimb.ras <- shp2raster(shp = IBRA_Kimb.shp, mask.raster = template.ras, value = 1, label="IBRA_kimb")
 
 output.extent <- extent(template.ras)
-output.extent@ymin <- -24
-output.extent@xmin <- 113
-output.extent@xmax <- 152
 
 IBRA_Kimb.ras <- crop(IBRA_Kimb.ras,output.extent)
 
 output_path     <- 'C:/Users/u3579238/Work/Refugia/Stability/Kimberley/'
 output_filename <- "IBRA_NorthKimberley.asc"
-writeRaster(IBRA_Kimb.ras, paste(output_path,output_filename), overwrite=TRUE)
+writeRaster(IBRA_Kimb.ras, paste(output_path,output_filename,sep=""), overwrite=TRUE)
 
 # Broad Kimberley
 region_list <- c("Northern Kimberley", "Central Kimberley", "Victoria Bonaparte")
@@ -73,16 +70,13 @@ IBRA_Kimb.shp  <- IBRA.shp[IBRA.shp$REG_NAME_7 %in% region_list,]
 IBRA_Kimb.ras <- shp2raster(shp = IBRA_Kimb.shp, mask.raster = template.ras, value = 1, label="IBRA_kimb")
 
 output.extent <- extent(template.ras)
-output.extent@ymin <- -24
-output.extent@xmin <- 113
-output.extent@xmax <- 152
 
 IBRA_Kimb.ras <- crop(IBRA_Kimb.ras,output.extent)
 plot(IBRA_Kimb.ras)
 
 output_path     <- 'C:/Users/u3579238/Work/Refugia/Stability/Kimberley/'
 output_filename <- "IBRA_BroadKimberley.asc"
-writeRaster(IBRA_Kimb.ras, paste(output_path,output_filename), overwrite=TRUE)
+writeRaster(IBRA_Kimb.ras, paste(output_path,output_filename,sep=""), overwrite=TRUE)
 
 # Narrow AMT
 region_list <- c("Arnhem Coast","Arnhem Plateau","Central Arnhem","Daly Basin","Darwin Coastal","Gulf Fall and Uplands",
@@ -92,13 +86,10 @@ IBRA_Kimb.shp  <- IBRA.shp[IBRA.shp$REG_NAME_7 %in% region_list,]
 IBRA_Kimb.ras <- shp2raster(shp = IBRA_Kimb.shp, mask.raster = template.ras, value = 1, label="IBRA_kimb")
 
 output.extent <- extent(template.ras)
-output.extent@ymin <- -24
-output.extent@xmin <- 113
-output.extent@xmax <- 152
 
 IBRA_Kimb.ras <- crop(IBRA_Kimb.ras,output.extent)
 plot(IBRA_Kimb.ras)
 
 output_path     <- 'C:/Users/u3579238/Work/Refugia/Stability/Kimberley/'
 output_filename <- "IBRA_NarrowAMT.asc"
-writeRaster(IBRA_Kimb.ras, paste(output_path,output_filename), overwrite=TRUE)
+writeRaster(IBRA_Kimb.ras, paste(output_path,output_filename,sep=""), overwrite=TRUE)
