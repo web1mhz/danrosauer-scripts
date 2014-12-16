0d8712365f289c9edd7822a1cad9019e24a58af0
danielrosauer <danielrosauer@394b1998-26ba-c760-0df3-ebdbcbab81c8>
2014-12-16 07:00:28 +0000

    Latest versions designed to run with a single input directory structure and multiple output directories.

--- trunk/Marxan/create_runscript.bat
+++ trunk/Marxan/create_runscript.bat
@@ -1,18 +1,18 @@
 #!/bin/bash
 shopt -s globstar
 
-for num in {2..100}
+for num in {1..100}
 do
-  dir=/home2/danr/marxan_mammals/ph_25cap/run_$num
+  dir=/home2/danr/marxan_mammals/sp_25cap_10pc/run_$num
   run_name=run$num
   echo $dir
 
   filename=runMarxan_25pc.sh
   file=$dir/$filename
 
-  echo "#$ -N "$run_name"_ph" > $file
+  echo "#$ -N "$run_name"_sp10" > $file
   echo "#$ -pe threads 1" >> $file
-  echo "#$ -l virtual_free=0.5g,h_vmem=0.6g" >> $file
+  echo "#$ -l virtual_free=0.5g,h_vmem=0.55g" >> $file
   echo "#$ -j y" >> $file
   echo "#$ -M dan.rosauer@anu.edu.au" >> $file
   echo "#$ -m bae" >> $file
