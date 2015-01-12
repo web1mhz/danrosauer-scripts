85120afeacd0d1d262d86cb8aa149a181b1b10cd
danielrosauer <danielrosauer@394b1998-26ba-c760-0df3-ebdbcbab81c8>
2015-01-12 05:04:46 +0000

    Lots of edits to fix errors and clean code for easy reading

--- trunk/sPhy/prepare_marxan_inputs.r
+++ trunk/sPhy/prepare_marxan_inputs.r
@@ -14,94 +14,57 @@ MarxanInputs <- function(
                   spf_multiplier) {
 
   cat("\n\nGenerating marxan outputs for",write.dir,"\n\n")
-  
+
   cons.feature.count <- nrow(branch_data)
   branch_ranges   <- as.numeric(branch_data$BranchRange)
   branch_names    <- as.character(labels(tree))
   branch_IDs      <- as.numeric(getNode(tree))
-  
+
   if (target=='func') {
     branch_targets <- ceiling(get_target(branch_ranges,1))
   } else {
-    branch_targets  <- ceiling(branch_ranges * target)  
+    branch_targets  <- ceiling(branch_ranges * target)
   }
-  
-  branch_info     <- as.data.frame(print(tree))
-  branch_lengths  <- branch_info$edge.length
+
+  branch_lengths  <- edgeLength(tree, branch_IDs)
   branch_lengths[which(is.na(branch_lengths))] <- 0
   spec <- data.frame(cbind(branch_IDs,branch_targets,round(branch_lengths * spf_multiplier,4),branch_names))
   names(spec) <- c("species_id","target","spf","name")
-  #spec <- spec[-which(spec$spf==0),]
-  
-  if (class(node_occ)=="matrix") {
-    
-    pu.count     <- nrow(quad_list)
-    cost <- rep(1,pu.count)  # SETS ALL COSTS TO 1.  VARYING COSTS CAN BE ADDED LATER
-    pu_list <- cbind(quad_list$cell,quad_list$cell,cost)
-    names(pu_list) <- c("pu_id","pu_name","LandProportion")
-    pu <- data.frame(pu_list[,c(1,3)])
-    names(pu) <- c("id","cost")
-    
-    node_by_cell <- melt(node_occ)
-    names(node_by_cell) <- c("pu","node","amount")
-    node_by_cell <- node_by_cell[node_by_cell$amount > 0,c(2,1,3)]
-    node_by_cell_SpID <- merge(spec, node_by_cell, by.x="name", by.y="node")
-    puvspr2 <- data.frame(node_by_cell_SpID$species_id,node_by_cell_SpID$pu,node_by_cell_SpID$amount)
-    names(puvspr2) <- c("species","pu","amount")  # this data frame has the species number, but the PU name
-    rm(node_by_cell_SpID)
-    #puvspr2$species <- as.numeric(puvspr2$species)  
-    
-    # now order files to use Marxan quick preparation method
-    puvspr2 <- puvspr2[order(puvspr2$pu),]
-    sporder <- puvspr2[order(puvspr2$species),]
-    
-    # ensure correct column names for maxent format
-    names(spec)[1] <- "id"
-    
-  } else {
-    
-    pu_from_occ  <- data.frame(sort(unique(node_occ[,2])))
-    names(pu_from_occ)  <- "QuadID"
-    names(quad_list)[1] <- "QuadID"
-  
-    pu_list      <- merge(pu_from_occ,quad_list,by="QuadID", all=F)
-    pu.count     <- nrow(pu_list)
-  
-    pu_list <- data.frame(cbind(rep(1:pu.count),pu_list))
-    names(pu_list) <- c("pu_id","pu_name","LandProportion")
-  
-    pu <- data.frame(pu_list[,c(1,3)])
-    names(pu) <- c("id","cost")
-    node_by_cell_SpID <- merge(spec, node_occ, by.x="species_id", by.y="BranchID")
-    puvspr2 <- data.frame(node_by_cell_SpID$species_id,node_by_cell_SpID$QuadID,node_by_cell_SpID$Proportion)
-    names(puvspr2) <- c("species","pu_name","amount")  # this data frame has the species number, but the PU name
-    rm(node_by_cell_SpID)
-  
-    puvspr2 <- merge(pu_list, puvspr2, by="pu_name")
-    puvspr2 <-  puvspr2[,c("species","pu_id","amount")]
-  
-    names(puvspr2) <- c("species","pu","amount")
-    puvspr2$species <- as.numeric(puvspr2$species)	
-    
-    # now order files to use Marxan quick preparation method
-    puvspr2 <- puvspr2[order(puvspr2$pu),]
-    sporder <- puvspr2[order(puvspr2$species),]
-    
-    # ensure correct column names for maxent format
-    names(spec)[1] <- "id"
-  }
+
+  pu.count     <- nrow(quad_list)
+  cost <- rep(1,pu.count)  # SETS ALL COSTS TO 1.  VARYING COSTS CAN BE ADDED LATER
+  pu_list <- cbind(quad_list$cell,quad_list$cell,cost)
+  names(pu_list) <- c("pu_id","pu_name","LandProportion")
+  pu <- data.frame(pu_list[,c(1,3)])
+  names(pu) <- c("id","cost")
+
+  node_by_cell <- melt(node_occ)
+  names(node_by_cell) <- c("pu","node","amount")
+  node_by_cell <- node_by_cell[node_by_cell$amount > 0,c(2,1,3)]
+  node_by_cell_SpID <- merge(spec, node_by_cell, by.x="name", by.y="node")
+  puvspr2 <- data.frame(node_by_cell_SpID$species_id,node_by_cell_SpID$pu,node_by_cell_SpID$amount)
+  names(puvspr2) <- c("species","pu","amount")  # this data frame has the species number, but the PU name
+  rm(node_by_cell_SpID)
+  #puvspr2$species <- as.numeric(puvspr2$species)
+
+  # now order files to use Marxan quick preparation method
+  puvspr2 <- puvspr2[order(puvspr2$pu),]
+  sporder <- puvspr2[order(puvspr2$species),]
+
+  # ensure correct column names for maxent format
+  names(spec)[1] <- "id"
 
   try(dir.create(write.dir),silent = TRUE)
 
   cat("\nAbout to write files to ",write.dir,"\n")
-  
+
   setwd(write.dir)
   write.csv(spec,"spec.csv",row.names=F)
   write.csv(pu,"pu.csv",row.names=F)
   write.csv(puvspr2,"puvspr2.csv",row.names=F)
   write.csv(sporder,"sporder.csv",row.names=F)
   write.csv(pu_list,"pu_id_lookup.csv",row.names=F)
-  
+
   return(1)
 }
 
@@ -124,12 +87,12 @@ get_target <- function(range, option=1) {
   # option 2: target = 50% if < 20 cells, 10 cells if > 20 cells
   # option 3: target = 25% if less than 100 cells, then a flat 25.  Minimum target = 1
   # option 3: target = 100% if < 10 cells, 10 cells if < 100, 10% if < 2000, then flat at 200.
-  
+
   if (!(option %in% c(1,2,3,4))) {
     option <- 1
     cat("\nDefaulting to target option 1\n")
   }
-  
+
   n <- length(range)
   target <- vector(mode="numeric",length=n)
   if (option ==1) {
--- trunk/sPhy/run_scape_for_marxan.r
+++ trunk/sPhy/run_scape_for_marxan.r
@@ -3,6 +3,8 @@
 
 ##The simplified version omits the perturbations and missing information modules. Will add to later versions...
 
+## Links to generate files for PD conservation scenarios in Marxan - Dan Rosauer
+
 rm(list=ls())
 
 ##Load Libraries
@@ -14,147 +16,281 @@ require(geiger)
 require(phangorn)
 require(picante)
 require(entropart)
-
+require(phylobase)
 
 #directory for source files
-setwd("C:/Users/u3579238/Work/Software/danrosauer-scripts/sPhy")
-
-##Necessary source files (pls load all)
-
-##code for the wrapper functions that run the landscape simulation and the tree generation
-source('C:/Users/u3579238/Dropbox/sDiv S-Phy/groups/simulation/code/simulation/Working_Sims/script_to_run_scape5_simplified.R', chdir = TRUE)
+#setwd("~/Work/Software/danrosauer-scripts/sPhy")
 
 ##back end code for the scape script - generates landscapes using trees
-source('C:/Users/u3579238/Dropbox/sDiv S-Phy/groups/simulation/code/simulation/Working_Sims/gradient_cart_simulation_v3.1.R', chdir = TRUE)
+source('~/Dropbox/sDiv S-Phy/groups/simulation/code/simulation/Working_Sims/gradient_cart_simulation_v3.1.R', chdir = TRUE)
 
 ##plotting and landscape perturbation functions
-source('C:/Users/u3579238/Dropbox/sDiv S-Phy/groups/simulation/code/simulation/Working_Sims/landscape_manipulation_plotting.R', chdir = TRUE)
+source('~/Dropbox/sDiv S-Phy/groups/simulation/code/simulation/Working_Sims/landscape_manipulation_plotting.R', chdir = TRUE)
 
 ##generate trees with a variation of shapes
-source('C:/Users/u3579238/Dropbox/sDiv S-Phy/groups/simulation/code/simulation/Working_Sims/tree_generate_fct.R', chdir = TRUE)
+source('~/Work/Software/danrosauer-scripts/sPhy/tree_generate_fct.R', chdir = TRUE)
 
-#Meaning of scape types - 1st number is the input # for the runscape function, second # is the figure reference for each landscape type.
-	 
-	##Environment covary w phylogeny
-	#1 = #1a) ##lambda E = 1, lambda R = 0, repulsion
-	#weak attraction (i.e. env filtering), strong repulsion(competition), with no signal of range size
-
-	#2 = #1b) ##lambda E = 1, lambda R = 0, attraction
-	#strong attraction (i.e. env filtering), weak repulsion (competition), with no signal of range size
-
-	##Environment and range covary w phylogeny
-	#3 = #2a)##lambda E = 1, lambda R = 1, repulsion
-	
-	#4 = #2b)##lambda E = 1, lambda R = 1, attraction
-	
-	##No environment or range covariance with phylogeny
-	#5 = #3 spatial)##lambda E =0, lambda R = 0
-	
-	#6 = #3 aspatial)##lambda E =0, lambda R = 0
-	
-	##Range phylogeny covariance with no environmental
-	#signal
-	#7 = #4 spatial)  lambda E =0, lambda R = 1
-	
-	#8 = #4 aspatial)##lambda E =0, lambda R = 1,
+##create Marxan input files for PD conservation scenario
+source("~/Work/Software/danrosauer-scripts/sPhy/Prepare_Marxan_Inputs.r")
 
 
-###############USING SCAPE SETUP################################################
-####################Generate trees - creates 10 times as many trees as you ask for, then subsamples in order to increase variation.
+env_by_cell <- function(environmental_gradient,scape.size) {
+  size <- scape.size + 1 # currently the actual number of rows / columns is scape.size + 1
+  row_seq <- rep(environmentalgradient,times=size)
+  col_seq <- rep(environmentalgradient,each=size)
+  cell_env <- row_seq + col_seq
+  return(cell_env)
+}
 
-numsp= 64 	#specify number of species, must be power of 2
-numtrees=1 	#specify number of trees to generate
-tree_gen<-tree_generate(numsp,numtrees) 	#generate trees
+
+# Number of sims to run
+#  Each simulation has the same parameters, but unique:
+#   * tree
+#   * landscape
+#   * marxan runs
+number_of_sims <- 5
+
+# tree parameters
+tree_type = "rcoal"
+
+# scape parameters
+numsp            <-  64   #specify number of species, must be power of 2
+scape.size       <-  39
+g.center         <-  1
+g.range          <-  1
+g.repulse        <-  1
+wd.all           <-  ((scape.size+1)^2)*0.02
+signal.center    <-  TRUE
+signal.range     <-  TRUE
+same.range       <-  FALSE
+repulse          <-  FALSE
+center.scale     <-  1
+range.scale      <-  1
+repulse.scale    <-  1
+site.stoch.scale <-  0
+sd.center        <-  3
+sd.range         <-  1
+rho              <-  NULL
+th               <-  5
+maxabund         <-  10000
+#subsample.percent <- 75
+#detect.threshold  <- 5
+
+# marxan parameters
+base_dir          <- "C:/Users/u3579238/Work/Software/Marxan/scape_test/"
+first_dir_num     <- 1
+output_dir_prefix <- "run"
+plot_each_run <- T    # produce a pdf with richness map and range-size histogram
+target            <- 0.1  # proportion of the range of each branch to be protected
+spf_multiplier    <- 10   # Marxan parameter for importance of meeting targets
+                          # Marxan Species Protection Factor (SPF) = branch length x spf_multiplier
+
+# set up a metadata frame
+metadata <- data.frame(run_number = 0,
+                       marxan_dir = "",
+                       tree_type = "",
+                       tree_colless = 0,
+                       tree_gamma = 0,
+                       numsp = 0,
+                       scape.size = 0,
+                       cell.count = 0,
+                       g.center = 0,
+                       g.range = 0,
+                       g.repulse = 0,
+                       wd.all = 0,
+                       signal.center = FALSE,
+                       signal.range =  FALSE,
+                       same.range =  FALSE,
+                       repulse =  FALSE,
+                       center.scale = 0,
+                       range.scale = 0,
+                       repulse.scale = 0,
+                       site.stoch.scale = 0,
+                       sd.center = 0,
+                       sd.range = 0,
+                       th = 0,
+                       species_with_range = 0,
+                       occupied_cells = 0,
+                       mean_range_size = 0,
+                       mean_richness = 0,
+                       quantile_10_species_ranges = 0,
+                       date = "",
+                       stringsAsFactors=FALSE)
+
+# generate all trees and associated metadata
+tree_gen<-tree_generate(numsp, number_of_sims, type="average") 	#generate trees
 treeinfo<-tree_gen$treeinfo 	#save resulting tree info
 treelist<-tree_gen$treelist		#save resulting trees
+rm(tree_gen)
 
+# loop to generate Marxan input files for each simulation
+for (scenario_num in 1:number_of_sims) {
 
+  input_tree <- treelist[[scenario_num]]
 
-###################Generate scapes
-#uses default function values, allows you to change tree size, landscape size, specify landscape types to create
+  scape_out<-scape(input_tree,
+                   scape.size=scape.size,
+                   g.center=g.center,
+                   g.range=g.range,
+                   g.repulse=g.repulse,
+                   wd.all=wd.all,
+                   signal.center=signal.center,
+                   signal.range=signal.range,
+                   same.range=same.range,
+                   repulse=repulse,
+                   center.scale = center.scale,
+                   range.scale = range.scale,
+                   repulse.scale = repulse.scale,
+                   site.stoch.scale = site.stoch.scale,
+                   sd.center=sd.center,
+                   sd.range=sd.range,
+                   rho=rho,
+                   th=th)
 
-scape.size=100 	#specify landscape size (this is one edge of square matrix)
-total.cells=(scape.size+1)^2 
-maxabund=100 #what is the highest abundance (carrying capacity) possible in a patch?
-#subsample.percent=75	
-#detect.threshold=5
+  # note, rho currently has a value of NULL, and is not included in the metadata
 
-plotscapes=FALSE #do you want plots of all the landscapes generated? (say no if there are more than 3 or 4)
-scape.type=2 	#what scape types? "all" or c(1,2,3,4), etc
+  environmentalgradient<-seq(from=-1*(scape.size)/2,to=(scape.size)/2)
+  cell_env <- env_by_cell(environmentalgradient,scape.size)
 
-##create scapes
-Spmatrixlist <- runscape(numsp=numsp,scape.type=scape.type,scape.size=scape.size,treelist,maxabund=maxabund,plotscapes=FALSE) #generate a list of landscapes in the requested scape types using the supplied trees. 
+  ##use this output to create an abundance matrix and plots for the scape output.
+  species<-colnames(scape_out$Y)
+  abund_out<-landscape_abunds(scape_out,maxabund,species=species,figs=F)
+  PA_mat<-abund_out$PA_mat
 
-#######################################################
-# beyond here code is specific to Marxan preparation  #
-#######################################################
-
-library(phylobase)
-
-source("C:/Users/u3579238/Work/Software/danrosauer-scripts/sPhy/Prepare_Marxan_Inputs.r")
-
-# TEMP SET TO A SINGLE SCAPE DURING DEVELOPMENT
-scapenum          <- 1
-first_dir_num     <- 1
-base_dir          <- "C:/Users/u3579238/Work/Software/Marxan/scape_test/"
-output_dir        <- "run" 
-
-Spmatrix <- Spmatrixlist[scapenum][[1]]
-treenum <- Spmatrix[[3]]
-tree   <- phylo4(treelist[[treenum]])
-
-matrix_PA  <- Spmatrix[[2]]
-
-taxa <- dimnames(matrix_PA)[[2]]
-taxon_count <- length(taxa)
-node_count  <- nrow(tree@edge)
-cell_count <- nrow(matrix_PA)
-cells <- data.frame(cell = 1:cell_count,area = rep(1,cell_count))
-
-# expand the presence absence matrix to have a column for each internal node
-matrix_PA_nodes <- matrix(nrow=cell_count,ncol=node_count)
-matrix_PA_nodes[,1:taxon_count] <- matrix_PA
-
-# give names to internal branches (added to species names)
-node_labels <- paste("node_", taxon_count+1:nNodes(tree), sep="")
-nodeLabels(tree) <- node_labels
-
-nodes <- getNode(tree,1:node_count)
-dimnames(matrix_PA_nodes) <- list(NULL,names(nodes))
-BranchData  <- data.frame(BranchNames=names(nodes), BranchRange=as.numeric(rep(0,node_count)),stringsAsFactors=FALSE)
-BranchData$BranchRange[1:taxon_count] <- apply(matrix_PA_nodes[,1:taxon_count],MARGIN=2,FUN=sum)   
-
-# loop through the internal nodes to add an occurrence column for each to the presence/absence matric
-for (m in (taxon_count+1):node_count) {
-  
-  branch_name  <- names(descendants(tree, m, type="tips"))
-  branch_occ    <- apply((matrix_PA[,which(taxa %in% branch_name)]),MARGIN=1,FUN=max)
-  matrix_PA_nodes[,m] <- branch_occ
-  BranchData$BranchRange[m] <- sum(branch_occ)
-  
-  node <- as.integer(nodes[m])
-  
-  # progress output - remove once working
-  if (m %% 10 == 0) {
-    cat("\n",m,"of",node_count,"done")
-    cat("\n",names(nodes)[m],"\t",node_count,"\n")
+  #name the output folder and create if necessary
+  if (number_of_sims > 1) {
+    dir_number <- scenario_num + first_dir_num - 1  # allows for starting with a later number
+    this_output_dir <- paste(base_dir,output_dir_prefix,"_",dir_number ,sep="")
+  } else {
+    this_output_dir <- paste(base_dir,output_dir_prefix,sep="")
   }
+  if (! file.exists(this_output_dir)) {dir.create(this_output_dir)}
+
+  input_tree  <- phylo4(input_tree)
+
+  cat("\n\n**  Starting run", scenario_num, "**")
+  cat("\nNumber of species:", numsp)
+  cat("\nLandscape size:", scape.size + 1, "x", scape.size + 1)
+  cat("\nCells:", (scape.size + 1)^2 )
+
+  # remove zero range species
+  species_ranges <- apply(PA_mat, MARGIN = 2, FUN = sum)
+  zero_ranges <- which(species_ranges == 0)
+  PA_mat <- PA_mat[,-zero_ranges]
+  input_tree <- subset(input_tree, tips.exclude=zero_ranges)
+  species <- species[-zero_ranges]
+  species_ranges <- apply(PA_mat, MARGIN = 2, FUN = sum)
+
+  # some summary numbers
+  cell_count <- nrow(PA_mat)
+  cells <- data.frame(cell = 1:cell_count,area = rep(1,cell_count))
+  node_count  <- nrow(input_tree@edge)
+  species_count <- length(species)
+
+  richness       <- apply(PA_mat, 1, sum)
+  occupied_cells <- which(richness > 0)
+  occupied_cell_count <- length(occupied_cells)
+  cat("\nOccupied_cells: ", occupied_cell_count, "\t\t", round(100 * occupied_cell_count / ((scape.size + 1)^2), 1), "%", sep="" )
+
+  cat("\n\nSpecies with range:", species_count)
+
+  # PDF of richness map and range size histogram for each simulation
+  if (scenario_num == 1 | plot_each_run) {  # plot only first unless plot_each_run=TRUE
+
+    pdf_filename <- paste(this_output_dir, "inputs.pdf", sep = "/")
+    pdf(pdf_filename, 16, 8)
+
+    par(mfrow=c(1,2))
+
+    # species richness
+    col_richnessmap <- heatcol <- colorRampPalette(c("white", "yellow", "red"))
+    richness.matrix <- matrix(richness, nrow=scape.size+1, ncol=scape.size+1, byrow=TRUE)
+    image(richness.matrix, col = terrain.colors(15), main="Species Richness")
+
+    #species x area
+    hist(species_ranges, 12, ylab="Number of species",xlab="Number of sites",main="Species Area Relationship",col="lightgrey")
+
+    #tree
+    input_tree_4d <- phylo4d(input_tree, tip.data=species_ranges)
+    names(input_tree_4d@data) <- "Range size"
+    treePlot(input_tree_4d)
+
+    dev.off()
+  }
+
+  # expand the presence absence matrix to have a column for each internal node
+  PA_mat_nodes <- matrix(nrow=cell_count,ncol=node_count)
+  PA_mat_nodes[,1:species_count] <- PA_mat
+
+  # give names to internal branches (added to species names)
+  node_labels <- paste("node_", species_count+1:nNodes(input_tree), sep="")
+  nodeLabels(input_tree) <- node_labels
+
+  nodes <- getNode(input_tree,1:node_count)
+  dimnames(PA_mat_nodes) <- list(NULL,names(nodes))
+  BranchData  <- data.frame(BranchNames=names(nodes), BranchRange=as.numeric(rep(0,node_count)),stringsAsFactors=FALSE)
+  BranchData$BranchRange[1:species_count] <- apply(PA_mat_nodes[,1:species_count],MARGIN=2,FUN=sum)
+
+  # loop through the internal nodes to add an occurrence column for each to the presence/absence matric
+  for (m in (species_count+1):node_count) {
+
+    branch_name  <- names(descendants(input_tree, m, type="tips"))
+    branch_occ    <- apply((PA_mat[,which(species %in% branch_name)]),MARGIN=1,FUN=max)
+    PA_mat_nodes[,m] <- branch_occ
+    BranchData$BranchRange[m] <- sum(branch_occ)
+
+    node <- as.integer(nodes[m])
+
+  }
+
+  marxan_files_ok  <- MarxanInputs(
+                      write.dir      = this_output_dir,
+                      quad_list      = cells,
+                      tree           = input_tree,
+                      branch_data    = BranchData,
+                      node_occ       = PA_mat_nodes,
+                      target         = target,
+                      spf_multiplier = spf_multiplier)
+
+  metadata[scenario_num,] <-  c(run_number   =   scenario_num,
+                                marxan_dir   =   this_output_dir,
+                                tree_type    =   tree_type,
+                                tree_colless = treeinfo[scenario_num,"Colless"],
+                                tree_gamma   = treeinfo[scenario_num,"Gamma"],
+                                numsp        =   numsp,
+                                scape.size   =   scape.size,
+                                cell_count   =   cell_count,
+                                g.center,
+                                g.range,
+                                g.repulse,
+                                wd.all,
+                                signal.center,
+                                signal.range,
+                                same.range,
+                                repulse,
+                                center.scale,
+                                range.scale,
+                                repulse.scale,
+                                site.stoch.scale,
+                                sd.center,
+                                sd.range,
+                                th,
+                                species_with_range = species_count,
+                                occupied_cells = length(which(richness > 0)),
+                                mean_range_size = mean(species_ranges),
+                                mean_richness = mean(richness),
+                                quantile_10_species_ranges = quantile(species_ranges, 0.1),
+                                date  =  date()
+                                )
+
+  cat("\nScenario",scenario_num,"generated\n")
+
+
+  gc() # garbage collect from memory
+
 }
 
-#name the output folder
-if (scapenum > 1) {
-  directory_number <- scapenum + first_dir_num - 1  # allows for starting with a later number
-  this_output_dir <- paste(base_dir,output_dir,"_",directory_number ,sep="")
-} else {
-  this_output_dir <- paste(base_dir,output_dir,sep="")
-}
+metadata_filename <- paste(base_dir,"metadata.csv",sep="")
+write.csv(metadata, metadata_filename, row.names=F)
 
-result <- MarxanInputs(write.dir = this_output_dir,
-                       quad_list = cells,
-                       tree = tree,
-                       branch_data = BranchData,
-                       node_occ = matrix_PA_nodes,
-                       target =          0.1,
-                       spf_multiplier =  10)
-
-gc() # garbage collect from memory
+cat("\nMarxan files for ", number_of_sims, " simulations done!\n", date(),"\n", sep="")
