a2b7f4f80a9e4e7354380830870ea23ba656d74e
danielrosauer <danielrosauer@394b1998-26ba-c760-0df3-ebdbcbab81c8>
2015-01-18 12:31:48 +0000

    Update_DB_from_file_flexible.r now works for all fields and lookup ids from related tables.  Also handles NULLs better.  This involves new functions in DB_utilities.r

--- trunk/Database/delete_DB_records_flagged_in_file.r
+++ trunk/Database/delete_DB_records_flagged_in_file.r
@@ -1,7 +1,6 @@
 rm(list=ls())
 
 library(RMySQL)
-library(gdata)  # for reading excel files
 source("~/Work/Software/danrosauer-scripts/Database/DB_utilities.r")
 
 user   <- 'danr'
@@ -15,21 +14,18 @@ dbInfo   <- dbGetInfo(con)
 dbTables <- dbListTables(con)
 
 # load in a table and compare to the existing records
-#new.filename   <- "~/Dropbox/ARC Laureate/sample info/Database/ForUploading/Catalano_data_upload_Eremiascincus_14i15.csv"
 new.filename   <- "~/Dropbox/ARC Laureate/sample info/Database/ForUploading/Carlia_20141128_CN.csv"
 db.table       <- "specimen"
 matching_field <- "specimen_local_id"
 
-delete_flag_field <- "delete"
+delete_flag_field <- "Delete"
 
-#new.data       <- read.csv(new.filename, stringsAsFactors=F)
-new.data       <- read.xls(new.filename, sheet = "Carlia_20141128_CN", header=T, stringsAsFactors=F)
+new.data       <- read.csv(new.filename, stringsAsFactors=F)
 new.fields     <- names(new.data)
-new.ids        <- new.data[,matching_field]
 
-# confirm that the dlete flag field is in new data
+# confirm that the delete flag field is in new data
 if (! delete_flag_field %in% new.fields) {
-  err.text <- paste("\nDelete flag not in the new data:", missing_fields_new)
+  err.text <- paste("\nDelete flag not in the new data:", new.fields)
   stop(err.text)
 }
 
@@ -38,37 +34,27 @@ new.data <- subset(new.data, select = c(matching_field, delete_flag_field))
 delete.ids <- new.data[which(new.data[,delete_flag_field]==1), matching_field]
 
 # start a transaction
-#dbBegin(con)
-records.updated <- 0
+dbBegin(con)
+records.deleted <- 0
+
+cat("\nAbout to delete", length(delete.ids), "records")
 
 # loop through records which have an id
-for (id in extant.new.ids) {
+for (id in delete.ids) {
 
-  # create sql to select a matching record from the database - with fields_to_update
-  field_list_txt <- com_sep(fields_to_update)
+  # create sql to delete flagged records
 
-  select.SQL <- paste("SELECT ",  field_list_txt, " from ", db.table, " WHERE ", matching_field, " = ", id, ";", sep="")
-  result    <- dbSendQuery(con, select.SQL)
-  db_row.df <- dbFetch(result)
-  column_info <- dbColumnInfo(result)
+  delete.SQL <- paste("DELETE from ", db.table, " WHERE ", matching_field, " = ", id, ";", sep="")
+  result    <- dbSendQuery(con, delete.SQL)
   dbClearResult(result)
 
-  rows <- which(new.data[,matching_field]==id)
-  new_row.df <- new.data[rows,]
-
-  # create sql for update statement
-  field_value_txt <- value_list_sql(new_row.df, column_info, matching_field)
-
-  update.SQL <- paste("UPDATE ", db.table, " SET ", field_value_txt,
-                      " WHERE ",  matching_field, " = ", id, ";", sep="")
-  result <- dbSendQuery(con, update.SQL)
-
-  records.updated <- records.updated + 1
-  cat("\n", id, "\t", records.updated, "\n\n")
+  records.deleted <- records.deleted + 1
+  cat("\n", id, "\t", records.deleted, "\n")
 
   dbClearResult(result)
 }
 
+dbCommit(con)
+cat("\nDelete committed for", length(delete.ids), "records")
+
 dbDisconnect(con)
-
-
